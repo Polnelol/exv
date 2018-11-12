bf2d0c065052da4bbfc54b2ccde33b312d438370
Sergey <exvoto95@gmail.com>
2018-11-12 19:17:08 +0300

    markdown files fixexs

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,21 @@ module.exports = {
   /*
   ** Headers of the page
   */
+  modules: [
+    '@nuxtjs/markdownit'
+  ],
+
+  // [optional] markdownit options
+  // See https://github.com/markdown-it/markdown-it
+  markdownit: {
+    preset: 'default',
+    linkify: true,
+    breaks: true,
+    use: [
+      // ['markdown-it-container', containerName],
+      // 'markdown-it-attrs'
+    ]
+  },
   head: {
     title: 'exv',
     meta: [
---
+++ pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-html="hello"></div>
+</template>
+
+<script>
+  import hello from '../static/hello.md'
+
+  export default {
+    computed: {
+      hello() {
+        return hello
+      }
+    }
+  }
+</script>
